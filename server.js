4fc63a3ac4acec2bc0000d253a981e3de81408c2
cmoi <cmoi@mail.fr>
2025-12-11 16:34:39 +0100

    stop server

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(port, () => {
+var server = app.listen(port, () => {
   console.log(`Application exemple à l'écoute sur le port ${port}!`);
+  
 });
+
+var handler = function() {
+  server.close();
+};
